57957864e872c908d0811dcd3ae57f63351e1408
kwaiidev <saceriojason@gmail.com>
2026-02-05 01:01:20 -0500

    analytics?

--- app/layout.tsx
+++ app/layout.tsx
@@ -3,6 +3,7 @@ import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 import Navbar from "./components/navbar";
 import Footer from "./components/Footer";
+import { Analytics } from "@vercel/analytics/react";
 
 const geistSans = Geist({
     variable: "--font-geist-sans",
@@ -25,6 +26,7 @@ export default function RootLayout({ children }: { children: React.ReactNode }) 
         <body className={`${geistSans.variable} ${geistMono.variable}`} suppressHydrationWarning={true}>
         <Navbar />
         {children}
+        <Analytics />
         <Footer />
         </body>
         </html>
